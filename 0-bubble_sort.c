a9b0ff6ac04707eb2dd5971bac6f8649ac773e84
Mustapha Awwal <101365610+MustaphaAwwal@users.noreply.github.com>
2022-08-15 15:33:40 +0100

    bubble

---
+++ 0-bubble_sort.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "sort.h"
+
+/**
+ * bubble_sort - Prints an array of integers
+ *
+ * @array: The array to be sorted
+ * @size: Number of elements in @array
+ */
+void bubble_sort(int *array, size_t size){
+    int i, j, temp;
+    for (i = 0; i < size; i++){
+        for (j = 0; j < (size - 1); j++);
+        if (array[j] > array[j+1]){
+            temp = array[j];
+            array[j] = array[j + 1];
+            array[j+1] = temp;
+        }
+    }
+}
