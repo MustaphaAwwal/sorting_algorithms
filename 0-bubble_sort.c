323df173431af2144fd8017f295b0a8a6a478483
Mustapha Awwal <101365610+MustaphaAwwal@users.noreply.github.com>
2022-08-15 20:49:57 +0100

    Update 0-bubble_sort.c

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -7,27 +7,31 @@
  * @array: The array to be sorted
  * @size: Number of elements in @array
  */
-void bubble_sort(int *array, size_t size){
+void bubble_sort(int *array, size_t size)
+{
     int i, j, temp, k;
-    
-    k = size;
-    for (i = 0; i < k; i++){
-        for (j = 0; j < k - 1; j++)
-        {
-               if (array[j] > array[j+1])
-                {
-                    temp = array[j];
-                    array[j] = array[j + 1];
-                    array[j+1] = temp;
-                }
-                if (j == k - 2){
-                    printf("%d", array[j]);
-                }
-                else{
-                    printf("%d", array[j]);
-                    printf(", ");
-                }
-        }
-        printf("\n");
-    }
+
+	k = size;
+	for (i = 0; i < k ; i++)
+	{
+		for (j = 0; j < k - 1; j++)
+		{
+			if (array[j] > array[j+1])
+			{
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j+1] = temp;
+			}
+			if (j == k - 2)
+			{
+				printf("%d", array[j]);
+			}
+			else
+			{
+				printf("%d", array[j]);
+				printf(", ");
+			}
+		}
+		printf("\n");
+	}
 }
