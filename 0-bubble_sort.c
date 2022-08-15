eae77f4fbff47bbb6369379ca82e410a822e373b
Mustapha Awwal <101365610+MustaphaAwwal@users.noreply.github.com>
2022-08-15 18:04:41 +0100

    Update 0-bubble_sort.c

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -12,12 +12,20 @@ void bubble_sort(int *array, size_t size){
     for (i = 0; i < size; i++){
         for (j = 0; j < size - 1; j++)
         {
-                if (array[j] > array[j+1])
+               if (array[j] > array[j+1])
                 {
                     temp = array[j];
                     array[j] = array[j + 1];
                     array[j+1] = temp;
                 }
+                if (j == size - 2){
+                    printf("%d", array[j]);
+                }
+                else{
+                    printf("%d", array[j]);
+                    printf(", ");
+                }
         }
+        printf("\n");
     }
 }
